3f59b36c1557ae559b59b677ff1045268b56b974
Piotr G <81086063+piogru@users.noreply.github.com>
2024-08-08 13:02:47 +0200

    feat: Create basic routing structure

---
+++ src/features/core/routes/Routes.tsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../../../App.tsx";
+import ErrorPage from "../components/ErrorPage.tsx";
+import Profile from "../../profiles/components/Profile.tsx";
+import NavbarWrapper from "../components/NavbarWrapper.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <NavbarWrapper />,
+        children: [
+          {
+            path: ":accountName",
+            element: <Profile />,
+            children: [
+              {
+                path: "",
+                element: <div>Posts</div>,
+              },
+              {
+                path: "saved",
+                element: <div>Saved</div>,
+              },
+            ],
+          },
+          {
+            path: "explore",
+            element: <div>Explore</div>,
+          },
+        ],
+      },
+
+      {
+        path: "/accounts/login",
+        element: <div>Login</div>,
+      },
+      {
+        path: "/accounts/signup",
+        element: <div>Signup</div>,
+      },
+    ],
+  },
+]);
+
+export default function Routes() {
+  return <RouterProvider router={router} />;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import BaseRoutes from "./BaseRoutes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Routes from "./features/core/routes/Routes.tsx";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BaseRoutes />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
