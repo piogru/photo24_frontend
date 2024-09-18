cb8476d2a6d36841132ff8b6a874addeb425d106
Piotr G <81086063+piogru@users.noreply.github.com>
2024-09-18 12:10:57 +0200

    feat: Display remaining post information in PostModal

--- src/features/core/components/PostModal.tsx
+++ src/features/core/components/PostModal.tsx
@@ -3,7 +3,7 @@ import Post from "../types/post";
 import Modal from "./Modal";
 import UserBar from "./UserBar";
 import { Button } from "@headlessui/react";
-import { formatDistanceStrict } from "date-fns";
+import { format, formatDistanceToNowStrict } from "date-fns";
 import {
   BookmarkIcon,
   ChatBubbleOvalLeftIcon,
@@ -25,13 +25,14 @@ export default function PostModal({ post, isOpen, onClose }: PostModalProps) {
   const photos = post.photos || [];
   const comments = post.comments;
   const currentPhoto = photos[currentPhotoIndex];
-  const timePosted = formatDistanceStrict(
+  const createdAt = format(post.createdAt, "MMM dd, yyyy");
+  const timePostedSuffix = formatDistanceToNowStrict(new Date(post.createdAt), {
+    addSuffix: true,
+  });
+  const distanceToPosted = formatDistanceToNowStrict(
     new Date(post.createdAt),
-    new Date(),
-    {
-      addSuffix: true,
-    },
-  );
+  ).split(" ");
+  const timePosted = `${distanceToPosted[0]} ${distanceToPosted[1][0]}`;
 
   const handlePrevious = () => {
     if (currentPhotoIndex > 0) {
@@ -45,12 +46,17 @@ export default function PostModal({ post, isOpen, onClose }: PostModalProps) {
     }
   };
 
+  const onFollowClick = () => {};
+
+  console.log(post);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="w-full max-w-[64rem] h-[30rem] flex flex-row justify-center items-center rounded-b-xl transition">
+      <div className="w-full lg:max-w-[80rem] xl:max-w-[92rem] h-[calc(100vh-theme(space.10))] flex flex-row justify-center items-center rounded-b-xl transition">
         {currentPhoto ?
-          <div className="h-full flex flex-col items-center justify-center border-r border-slate-300 dark:border-slate-600">
-            <div className="relative">
+          // todo: aspect in div below
+          <div className="max-w-5xl h-full flex flex-col flex-grow items-center justify-center border-r border-slate-300 dark:border-slate-600">
+            <div className="relative w-full">
               <img
                 src={currentPhoto.url}
                 alt={currentPhoto.altText}
@@ -73,71 +79,110 @@ export default function PostModal({ post, isOpen, onClose }: PostModalProps) {
                   <ChevronRightIcon className="size-6" />
                 </Button>
               : null}
-              {photos.length > 0 ?
-                <div className="absolute bottom-3 w-full flex flex-row justify-center items-center gap-0.5">
+
+              {photos.length > 1 ?
+                <div className="absolute bottom-3 w-full flex flex-row justify-center items-center gap-1">
                   {[...Array(photos.length).keys()].map((item) => (
                     <div
                       key={item}
-                      className={`size-1.5 rounded-full transition ${item === currentPhotoIndex ? "bg-gray-200 " : "bg-gray-500"}`}
-                    ></div>
+                      className={`size-1.5 rounded-full transition ${item === currentPhotoIndex ? "bg-gray-200 " : "bg-gray-400"}`}
+                    />
                   ))}
                 </div>
               : null}
             </div>
           </div>
         : null}
-        <div className="w-full h-full max-w-md flex-grow flex flex-col">
-          <div className="w-full flex flex-row justify-between items-center gap-2 border-b border-slate-300 dark:border-slate-600">
-            <div className="flex flex-row items-center gap-1 p-1">
-              {post.user ?
-                <UserBar user={post.user} />
-              : null}
-              <span>{"•"}</span>
-              <Button className="text-sm text-blue-500 hover:text-gray-800 dark:hover:text-gray-200">
-                Follow
-              </Button>
-            </div>
-            <Button>
-              <EllipsisHorizontalIcon className="size-6" />
-            </Button>
-          </div>
-          <div className="flex-grow border-b border-slate-300 dark:border-slate-600">
-            <div className="flex flex-row">
-              <div className="size-10 rounded-full bg-gray-500"></div>
-              <div>
-                {post?.user?.name} {post.caption}
+        <div className="w-80 xl:w-[28rem] h-full flex flex-row">
+          <div className="w-full h-full flex-grow flex flex-col">
+            <div className="w-full flex flex-row justify-between items-center gap-2 px-3 py-2 border-b border-slate-300 dark:border-slate-600">
+              <div className="flex flex-row items-center gap-1">
+                {post.user ?
+                  <UserBar user={post.user} />
+                : null}
+                <span>{"•"}</span>
+                <Button
+                  onClick={onFollowClick}
+                  className="text-sm text-blue-500 hover:text-gray-800 dark:hover:text-gray-200"
+                >
+                  {false ? "Following" : "Follow"}
+                </Button>
               </div>
-              <div>{post.createdAt}</div>
-            </div>
-            {comments.map((comment) => (
-              <div key={comment._id}>Comment component</div>
-            ))}
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row items-center gap-2">
-              <Button title="Like">
-                <HeartIcon className="size-6" />
-              </Button>
-              <Button title="Comment">
-                <ChatBubbleOvalLeftIcon className="size-6" />
-              </Button>
-              <Button title="Share">
-                <ShareIcon className="size-6" />
+              <Button className="hidden">
+                <EllipsisHorizontalIcon className="size-6" />
               </Button>
             </div>
-            <div>
-              <Button title="Save">
-                <BookmarkIcon className="size-6" />
-              </Button>
-            </div>
-          </div>
 
-          <div className="flex flex-col">
-            <div className="">{post.likes} likes</div>
-            <div className="text-sm">{timePosted}</div>
-          </div>
+            <div className="flex flex-col flex-grow px-3 py-4 gap-3 overflow-y-auto border-b border-slate-300 dark:border-slate-600">
+              <div className="flex flex-row gap-3">
+                <div className="size-8 rounded-full flex-shrink-0 bg-gray-500" />
+                <div className="flex flex-col justify-start gap-1">
+                  <div className="text-start whitespace-pre-line">
+                    <span className="inline-flex font-semibold">
+                      {post?.user?.name} Account name
+                    </span>{" "}
+                    <p className="inline">{post.caption}</p>
+                  </div>
+                  <div
+                    title={createdAt}
+                    className="text-xs text-gray-800 dark:text-gray-400"
+                  >
+                    {timePosted}
+                  </div>
+                </div>
+              </div>
+              {comments.map((comment) => (
+                <div key={comment._id}>Comment component</div>
+              ))}
+            </div>
 
-          <div>New comment</div>
+            <div className="flex flex-row justify-between px-3 py-2">
+              <div className="flex flex-row items-center gap-2">
+                <Button
+                  title="Like"
+                  className="text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-400
+                  active:text-gray-700 dark:active:text-gray-500"
+                >
+                  <HeartIcon className="size-7" />
+                </Button>
+                <Button
+                  title="Comment"
+                  className="text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-400
+                  active:text-gray-700 dark:active:text-gray-500"
+                >
+                  <ChatBubbleOvalLeftIcon className="size-7" />
+                </Button>
+                <Button
+                  title="Share"
+                  className="text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-400
+                  active:text-gray-700 dark:active:text-gray-500"
+                >
+                  <ShareIcon className="size-7" />
+                </Button>
+              </div>
+              <div>
+                <Button
+                  title="Save"
+                  className="text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-400
+                  active:text-gray-700 dark:active:text-gray-500"
+                >
+                  <BookmarkIcon className="size-7" />
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex flex-col px-3 pb-2 border-b border-slate-300 dark:border-slate-600">
+              <div className="font-semibold">{post.likes} likes</div>
+              <div
+                title={createdAt}
+                className="text-xs text-gray-800 dark:text-gray-400"
+              >
+                {timePostedSuffix}
+              </div>
+            </div>
+
+            <div className="px-3 py-2">New comment</div>
+          </div>
         </div>
       </div>
     </Modal>
--- src/features/core/components/UserBar.tsx
+++ src/features/core/components/UserBar.tsx
@@ -7,14 +7,14 @@ type UserBarProps = {
 
 export default function UserBar({ user }: UserBarProps) {
   return (
-    <div className="flex flex-row items-center gap-2">
+    <div className="flex flex-row items-center gap-3">
       <NavLink to={`/${user?.name}`}>
         {user?.avatar?.url ?
           <img src={user.avatar.url} className="size-8 rounded-full" />
         : <div className="size-8 rounded-full bg-gray-500" />}
       </NavLink>
       <NavLink to={`/${user?.name}`}>
-        <div>Acc name</div>
+        <div className="font-semibold">Acc name</div>
       </NavLink>
     </div>
   );
