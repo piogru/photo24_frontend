1e7cdd2b4878ab3de6ac142c45a4de46c1f6a2c9
Piotr G <81086063+piogru@users.noreply.github.com>
2024-09-20 13:17:10 +0200

    fix: Add safeguard against null post.user in postDetailsLoader

--- src/features/core/api/loaders.ts
+++ src/features/core/api/loaders.ts
@@ -64,7 +64,7 @@ export const postDetailsLoader =
         return null;
       });
     const follow =
-      post ?
+      post?.user ?
         await queryClient
           .ensureQueryData(followQuery(post.user._id))
           .then((data) => {
