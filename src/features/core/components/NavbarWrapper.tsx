efe14943c9a43bbc9eb25206e2107e66c3e2a00a
Piotr G <81086063+piogru@users.noreply.github.com>
2024-09-27 11:33:43 +0200

    fix: <main> not adjusting width when navbar changes

--- src/features/core/components/NavbarWrapper.tsx
+++ src/features/core/components/NavbarWrapper.tsx
@@ -6,7 +6,7 @@ export default function NavbarWrapper() {
   return (
     <div className="flex min-h-screen">
       <Navbar />
-      <section className="min-h-full max-h-screen w-[calc(100%-240px)] flex flex-col overflow-y-auto">
+      <section className="min-h-full max-h-screen w-full xl:w-[calc(100%-240px)] flex flex-col overflow-y-auto">
         <main className="flex-grow">
           <Outlet />
         </main>
