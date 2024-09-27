67d45237abfb1336e40ba09372e40185c1e518eb
Piotr G <81086063+piogru@users.noreply.github.com>
2024-09-27 14:46:20 +0200

    feat: Make Feed responsive on all width breakpoints

--- src/features/posts/components/PostPreview.tsx
+++ src/features/posts/components/PostPreview.tsx
@@ -62,7 +62,7 @@ export default function PostPreview({ post }: PostProps) {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex flex-row justify-between items-center">
+      <div className="flex flex-row justify-between items-center px-3 sm:px-0">
         <div className="flex flex-row items-center gap-1">
           <UserBar user={post?.user} followEnabled={false} />
           <span>{"•"}</span>
@@ -86,52 +86,53 @@ export default function PostPreview({ post }: PostProps) {
           </div>
         : null}
       </div>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-row items-center gap-2">
-          <IconButton
-            Icon={HeartIcon}
-            SolidIcon={HeartIconSolid}
-            solid={!!like}
-            title={like ? "Unlike" : "Like"}
-            onClick={onLikeClick}
-          />
-          <IconButton
-            disabled
-            Icon={ChatBubbleOvalLeftIcon}
-            title="Comment"
-            onClick={onCommentClick}
-          />
-          <IconButton
-            disabled
-            Icon={ShareIcon}
-            title="Share"
-            onClick={onShareClick}
-          />
+
+      <div className="flex flex-col gap-2 px-3 sm:px-0">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row items-center gap-2">
+            <IconButton
+              Icon={HeartIcon}
+              SolidIcon={HeartIconSolid}
+              solid={!!like}
+              title={like ? "Unlike" : "Like"}
+              onClick={onLikeClick}
+            />
+            <IconButton
+              disabled
+              Icon={ChatBubbleOvalLeftIcon}
+              title="Comment"
+              onClick={onCommentClick}
+            />
+            <IconButton
+              disabled
+              Icon={ShareIcon}
+              title="Share"
+              onClick={onShareClick}
+            />
+          </div>
+          <div>
+            <IconButton
+              disabled
+              Icon={BookmarkIcon}
+              SolidIcon={BookmarkIconSolid}
+              solid={false}
+              title="Save"
+              onClick={onSaveClick}
+            />
+          </div>
         </div>
-        <div>
-          <IconButton
-            disabled
-            Icon={BookmarkIcon}
-            SolidIcon={BookmarkIconSolid}
-            solid={false}
-            title="Save"
-            onClick={onSaveClick}
-          />
-        </div>
+        <span>
+          <span className="font-semibold">{post.likes}</span> likes
+        </span>
+        {post.caption?.length > 0 ?
+          <div>
+            <span className="inline mr-1 font-semibold">
+              {post.user?.name || "user"}
+            </span>
+            <ShowMoreText text={post.caption} overflowLength={100} />
+          </div>
+        : null}
       </div>
-
-      <span>
-        <span className="font-semibold">{post.likes}</span> likes
-      </span>
-
-      {post.caption?.length > 0 ?
-        <div>
-          <span className="inline mr-1 font-semibold">
-            {post.user?.name || "user"}
-          </span>
-          <ShowMoreText text={post.caption} overflowLength={100} />
-        </div>
-      : null}
     </div>
   );
 }
