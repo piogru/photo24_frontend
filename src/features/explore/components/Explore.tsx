776d9fe6190bd03d130eee2d3fca83a2242243ee
Piotr G <81086063+piogru@users.noreply.github.com>
2024-09-27 14:57:24 +0200

    chore: Fix Explore content overflowing layout below sm breakpoint

--- src/features/explore/components/Explore.tsx
+++ src/features/explore/components/Explore.tsx
@@ -48,9 +48,9 @@ export default function Explore() {
         />
       : null}
 
-      <div className="flex flex-col justify-center items-center pt-12 pb-4 mx-4">
-        <div className="min-w-64 w-full max-w-4xl">
-          <div className="w-full flex flex-row justify-start items-center gap-3 font-bold">
+      <div className="flex flex-col justify-center items-center pt-12 pb-4 mx-0 sm:mx-4">
+        <div className="w-full max-w-4xl">
+          <div className="w-full flex flex-row justify-start items-center gap-3 font-bold mx-2 sm:mx-0">
             <NavLink to="" end className="py-2">
               {({ isActive }) => (
                 <span
@@ -64,7 +64,7 @@ export default function Explore() {
           <div className="flex flex-col items-center gap-4">
             {postsLoading ?
               <Spinner />
-            : <div className="w-full grid grid-cols-3 gap-1">
+            : <div className="w-full grid grid-cols-3 gap-0.5 sm:gap-1 ">
                 {allPosts?.map((post) => (
                   <PostSquare
                     key={post._id}
