85d3e814e08afe8696cbebb34e77f9ed2d0983fe
piogru <81086063+piogru@users.noreply.github.com>
2024-10-01 11:03:46 +0200

    refactor: Extract theme state control to ThemeContext (#28)

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Routes from "./features/core/routes/Routes.tsx";
 import queryClient from "./features/core/api/queryClient.ts";
+import ThemeContextProvider from "./features/core/components/ThemeContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Routes />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeContextProvider>
   </React.StrictMode>,
 );
