4cce807a47ab31ef63b34aa043fdc9e7efe56171
Piotr G <81086063+piogru@users.noreply.github.com>
2024-10-04 12:59:46 +0200

    chore: Add APP_VERSION and BUILD_DATE to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+  },
   test: {
     globals: true,
     environment: "jsdom",
