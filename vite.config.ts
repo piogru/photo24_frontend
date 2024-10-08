d2dff908eae93de6a20ced24cc4cc1c1cd78e2a3
Piotr G <81086063+piogru@users.noreply.github.com>
2024-10-08 20:28:29 +0200

    chore: Remove npm_package_version plugin from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
   },
   test: {
