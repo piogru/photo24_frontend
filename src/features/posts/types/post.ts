d74c0d418ae0a079518e839845f56aa83522cc07
Piotr G <81086063+piogru@users.noreply.github.com>
2024-11-13 13:47:23 +0100

    fix: Change import paths in Post type declaration

--- src/features/posts/types/post.ts
+++ src/features/posts/types/post.ts
@@ -1,7 +1,7 @@
-import Comment from "./comment";
-import ObjectId from "./objectId";
-import Photo from "./photo";
-import User from "./user";
+import Comment from "../../core/types/comment";
+import ObjectId from "../../core/types/objectId";
+import Photo from "../../core/types/photo";
+import User from "../../core/types/user";
 
 type Post = {
   _id: ObjectId;
