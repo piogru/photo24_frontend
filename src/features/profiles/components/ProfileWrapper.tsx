88849c3a0a6971b99836eeb5d8cdfe2b30eaef6a
Piotr G <81086063+piogru@users.noreply.github.com>
2024-11-15 15:38:51 +0100

    refactor(profile): Move user information to separate component, improve selected tab detection

--- src/features/profiles/components/ProfileWrapper.tsx
+++ src/features/profiles/components/ProfileWrapper.tsx
@@ -3,14 +3,14 @@ import { profileLoader } from "../api/loaders";
 import Profile from "./Profile";
 
 export default function ProfileWrapper() {
-  const { user, follow } = useLoaderData() as Awaited<
+  const { user } = useLoaderData() as Awaited<
     ReturnType<ReturnType<typeof profileLoader>>
   >;
 
   return (
     <>
       {user ?
-        <Profile initialData={{ user, follow }} />
+        <Profile />
       : <div className="mt-12 flex flex-col items-center justify-center gap-4">
           <div className="text-2xl">This page is not available.</div>
           <Link to="/" className="text-lg text-blue-500">
