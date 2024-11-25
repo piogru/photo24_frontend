b563e6baf36bdfffe93649dc1e048a0a541d50ea
Piotr G <81086063+piogru@users.noreply.github.com>
2024-11-25 12:14:27 +0100

    chore: Move heading element for PostWizard closer to text, make clickable area for header nav buttons bigger

--- src/features/postCreate/components/MultiStepFormHeader.tsx
+++ src/features/postCreate/components/MultiStepFormHeader.tsx
@@ -14,41 +14,41 @@ export default function MultiStepFormHeader({
   const actions = useFormActions();
   const isNavigationVisible = currentStepIndex > 0 && currentStepIndex <= 2;
 
-  // TODO: move heading to text
   return (
-    <div>
-      <DialogTitle
-        className="inline-flex w-full flex-row items-center justify-between border-b
-          border-gray-100 px-3 py-1 text-center text-lg font-semibold dark:border-gray-900"
-      >
-        {isNavigationVisible ?
-          <Button
-            onClick={() => {
-              if (currentStepIndex === 1) {
-                openDiscard();
-              } else {
-                actions.prevStep();
-              }
-            }}
-          >
-            <ArrowLeftIcon className="size-6 dark:text-white" />
-          </Button>
-        : null}
-        <span className="mx-auto">{stepNames[currentStepIndex]}</span>
-        {isNavigationVisible ?
-          <Button
-            onClick={() => {
-              if (currentStepIndex < 2) {
-                actions.nextStep();
-              }
-            }}
-            type={currentStepIndex >= 2 ? "submit" : "button"}
-            className="text-base text-blue-500 hover:text-gray-700 dark:hover:text-gray-200"
-          >
-            {currentStepIndex === 2 ? "Share" : "Next"}
-          </Button>
-        : null}
+    <div
+      className="inline-flex h-10 w-full flex-row items-center justify-between border-b
+        border-gray-100 px-2 text-center text-lg font-semibold dark:border-gray-900"
+    >
+      {isNavigationVisible ?
+        <Button
+          onClick={() => {
+            if (currentStepIndex === 1) {
+              openDiscard();
+            } else {
+              actions.prevStep();
+            }
+          }}
+          className="p-2"
+        >
+          <ArrowLeftIcon className="size-6 dark:text-white" />
+        </Button>
+      : null}
+      <DialogTitle className="mx-auto text-center text-lg font-semibold dark:border-gray-900">
+        {stepNames[currentStepIndex]}
       </DialogTitle>
+      {isNavigationVisible ?
+        <Button
+          onClick={() => {
+            if (currentStepIndex < 2) {
+              actions.nextStep();
+            }
+          }}
+          type={currentStepIndex >= 2 ? "submit" : "button"}
+          className="px-2 py-1 text-base text-blue-500 hover:text-gray-700 dark:hover:text-gray-200"
+        >
+          {currentStepIndex === 2 ? "Share" : "Next"}
+        </Button>
+      : null}
     </div>
   );
 }
